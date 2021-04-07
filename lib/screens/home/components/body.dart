500361b91bccc31dd4c8022c8026f618eee91902
Zeeshan <jishanchoudhary@gmail.com>
2021-04-07 17:24:37 +0530

    second

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -11,7 +11,7 @@ class Body extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            //SizedBox(height: getProportionateScreenHeight(20)),
+            SizedBox(height: getProportionateScreenHeight(20)),
             HomeHeader(),
             SizedBox(height: getProportionateScreenWidth(10)),
             Categories(),
