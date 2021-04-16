7ecd1cee0c9256945a4b911c085b8602576b0de1
Zeeshan <jishanchoudhary@gmail.com>
2021-04-16 23:40:47 +0530

    second

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,13 +2,11 @@ import 'package:ComicMania/screens/comicss/comic.dart';
 import 'package:ComicMania/screens/comicss/solo/chap1/chap1.dart';
 import 'package:ComicMania/screens/creator_upload/upload.dart';
 import 'package:flutter/widgets.dart';
-import 'package:ComicMania/screens/cart/cart_screen.dart';
 import 'package:ComicMania/screens/complete_profile/complete_profile_screen.dart';
 import 'package:ComicMania/screens/details/details_screen.dart';
 import 'package:ComicMania/screens/forgot_password/forgot_password_screen.dart';
 import 'package:ComicMania/screens/home/home_screen.dart';
 import 'package:ComicMania/screens/login_success/login_success_screen.dart';
-import 'package:ComicMania/screens/otp/otp_screen.dart';
 import 'package:ComicMania/screens/profile/profile_screen.dart';
 import 'package:ComicMania/screens/sign_in/sign_in_screen.dart';
 import 'package:ComicMania/screens/splash/splash_screen.dart';
@@ -26,10 +24,8 @@ final Map<String, WidgetBuilder> routes = {
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
   SignUpScreen.routeName: (context) => SignUpScreen(),
   CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
-  OtpScreen.routeName: (context) => OtpScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
-  CartScreen.routeName: (context) => CartScreen(),
   ProfileScreen.routeName: (context) => ProfileScreen(),
   CreatorSignInScreen.routeName:(context)=> CreatorSignInScreen(),
   CreatorSignUpScreen.routeName:(context)=>CreatorSignUpScreen(),
